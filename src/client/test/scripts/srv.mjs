22ca0a795d16610c8133d2e77b0136a6e30880d9
hankei6km <gh-hankei6km@outlook.jp>
2023-11-06 09:56:42 +0000

    chore: Fix "Reflected cross-site scripting"

--- src/client/test/scripts/srv.mjs
+++ src/client/test/scripts/srv.mjs
@@ -1,29 +1,33 @@
-import * as http from "node:http";
-import { dataToNumberArray, getRouteAndArgs } from "./util.mjs";
+import * as http from 'node:http'
+import escapeHTML from 'escape-html'
+import { dataToNumberArray, getRouteAndArgs } from './util.mjs'
 
-const ipcHandlePath = process.env["IPC_HANDLE_PATH"];
+const ipcHandlePath = process.env['IPC_HANDLE_PATH']
 
-const server = http.createServer();
+const server = http.createServer()
 
-server.once("request", async (req, res) => {
-  const r = getRouteAndArgs(req.url);
-  if (req.method === "POST") {
-    const s1 = JSON.stringify({ id: 0, data: dataToNumberArray(r.route) });
-    await new Promise((resolve) => res.write(s1.slice(0, 3), resolve));
-    await new Promise((resolve) => res.write(s1.slice(3), resolve));
+server.once('request', async (req, res) => {
+  const r = getRouteAndArgs(req.url)
+  if (req.method === 'POST') {
+    const s1 = JSON.stringify({
+      id: 0,
+      data: dataToNumberArray(escapeHTML(r.route))
+    })
+    await new Promise((resolve) => res.write(s1.slice(0, 3), resolve))
+    await new Promise((resolve) => res.write(s1.slice(3), resolve))
 
     const s2 = JSON.stringify({
       id: 1,
-      data: dataToNumberArray(JSON.stringify(r.args)),
-    });
-    await new Promise((resolve) => res.write(s2.slice(0, 3), resolve));
-    await new Promise((resolve) => res.write(s2.slice(3), resolve));
+      data: dataToNumberArray(JSON.stringify(r.args.map((v) => escapeHTML(v))))
+    })
+    await new Promise((resolve) => res.write(s2.slice(0, 3), resolve))
+    await new Promise((resolve) => res.write(s2.slice(3), resolve))
 
-    res.end();
+    res.end()
   } else {
-    res.end("only POST method is allowed");
+    res.end('only POST method is allowed')
   }
-  server.close();
-});
+  server.close()
+})
 
-server.listen(ipcHandlePath || 3000);
+server.listen(ipcHandlePath || 3000)
